e77c6f6a8f0dc6688eac43b24fff720671dad187
Dolma Gurung <dolmatsering.gurung64@qmail.cuny.edu>
2020-07-15 00:49:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#python Programming
+#Basic Function Practice
+
+#Multiplying two numbers
+def multiply(num1, num2):
+  return print (num1 * num2)
+
+multiply(6,5)
+
+
+# Finding the max number among the three
+def max(x,y,z):
+  if x>y:
+    return x
+  else:
+    return y
+
+print(max (3,4,5))
